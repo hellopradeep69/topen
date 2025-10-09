9fc464db9c8ee93705a4a47e231a5b972ac2699f
k.pradeep <hellopradeep69@gmail.com>
2025-10-10 01:02:37 +0530

    inital commit

---
+++ topen.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+name="$1"
+
+case "$name" in
+btop)
+    btop
+    ;;
+ytdown)
+    ~/.local/bin/ytdown.sh
+    ;;
+lf)
+    lf ~/
+    ;;
+gitgo)
+    url=$(git remote get-url origin 2>/dev/null)
+
+    if [[ -z "$url" ]]; then
+        echo "Not in a repo folder"
+        notify-send -u normal ":-)"
+    else
+        xdg-open "$url"
+    fi
+    ;;
+*)
+    echo "Usage: topen.sh [OPTIONS] "
+    echo "Options:"
+    echo "  btop         Opens btop"
+    echo "  lf           Opens lf"
+    echo "  gitgo        Opens the currect repo in browser"
+    exit 0
+    ;;
+esac
